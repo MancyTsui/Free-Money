efb11c9bb7ee9fb8750375ced19f7c62f5b85c9c
MancyTsui <57313474+MancyTsui@users.noreply.github.com>
2022-03-30 08:41:57 +0800

    Update txstock.js

--- txstock.js
+++ txstock.js
@@ -41,7 +41,7 @@ MITM: wzq.tenpay.com
 35 11,16 * * * https://raw.githubusercontent.com/leafxcy/JavaScript/main/txstock.js, tag=腾讯自选股, enabled=true
 [rewrite_local]
 #获取APP和微信微证券的URL和header
-https://wzq.tenpay.com/cgi-bin/activity_task_daily.fcgi? url script-request-header https://raw.githubusercontent.com/leafxcy/JavaScript/main/txstock.js
+https://wzq.tenpay.com/cgi-bin/activity_task_daily.fcgi? url script-request-header https://raw.githubusercontent.com/MancyTsui/Free-Money/main/txstock.js
 [MITM]
 hostname = wzq.tenpay.com
 
